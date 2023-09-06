20bcc0d250f20ad10138656ca813e06b6edf7530
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-09-06 22:14:13 +0530

    Floder setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -3,10 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-bold text-indigo-500">Hello Discord</p>
-      <Button>
-        Click me
-      </Button>
+      This is a protected route
     </div>
   );
 }
