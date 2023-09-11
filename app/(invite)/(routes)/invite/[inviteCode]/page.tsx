f58c78c5146e868570a2db27be76343361573f83
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-09-11 21:14:09 +0530

    invite code continue

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,11 @@
+interface InviteCodePageProps {
+  params: {
+    inviteCode: string;
+  };
+}
+
+const InviteCodePage = ({ params }: InviteCodePageProps) => {
+  return <div className="">Hello</div>;
+};
+
+export default InviteCodePage;
